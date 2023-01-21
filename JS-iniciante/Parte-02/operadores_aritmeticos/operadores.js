553e97491ac7c5622d124c0f981127614dc1a901
Gabi170 <biele2611@gmail.com>
2023-01-21 14:12:08 -0300

    operadores logicos

---
+++ JS-iniciante/Parte-02/operadores_aritmeticos/operadores.js
@@ -0,0 +1,13 @@
+var num1 = 9
+var num2 = 2
+
+    console.log(`A soma entre ${num1} e ${num2} = ${num1 + num2}`)
+    console.log(`A subtração entre ${num1} e ${num2} = ${num1 - num2} `)
+    console.log(`A multiplicação entre ${num1} e ${num2} = ${num1 * num2}`)
+    console.log(`A divisão entre ${num1} e ${num2} = ${num1 / num2}`)
+    console.log(`O modulo entre ${num1} e ${num2} = ${num1 % num2}`)
+    console.log(`O incremento pre de ${num1} e ${num2} respectivamente é ${++num1} e ${++num2}`)
+    console.log(`O incremento pos de ${num1++} e ${num2++} respectivamente é ${num1} e ${num2}`)
+    console.log(`O decremento pre de ${num1} e ${num2} respectivamente é ${--num1} e ${--num2}`)
+    console.log(`O decremento pos de ${num1--} e ${num2--} respectivamente é ${num1} e ${num2}`)
+   
