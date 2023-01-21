54525ff07d962666e630528aa7f5d7be4dc3c543
Gabi170 <biele2611@gmail.com>
2023-01-21 13:18:52 -0300

    Desafio logica

---
+++ JS-iniciante/Parte-02/Desafio-IfElse/desafio.js
@@ -0,0 +1,14 @@
+ var idade = 55
+
+ if (idade <= 15){
+    console.log('É uma criança')
+ }
+ else if(idade >= 15 && idade <= 30){
+    console.log('É um jovem')
+ }
+ else if(idade <= 60){
+    console.log('É um adulto')
+ }
+ else{
+    console.log('É um idoso')
+ }
