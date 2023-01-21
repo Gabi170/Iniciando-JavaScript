89f99a7e0621b1acb84426a8a7f90071407abc11
Gabi170 <biele2611@gmail.com>
2023-01-21 14:48:58 -0300

    operadores

--- JS-iniciante/Parte-02/operadores_aritmeticos/operadores.js
+++ JS-iniciante/Parte-02/operadores_aritmeticos/operadores.js
@@ -1,13 +1,24 @@
-var num1 = 9
-var num2 = 2
+    var num1 = 9
+    var num2 = 2
 
-    console.log(`A soma entre ${num1} e ${num2} = ${num1 + num2}`)
-    console.log(`A subtração entre ${num1} e ${num2} = ${num1 - num2} `)
-    console.log(`A multiplicação entre ${num1} e ${num2} = ${num1 * num2}`)
-    console.log(`A divisão entre ${num1} e ${num2} = ${num1 / num2}`)
-    console.log(`O modulo entre ${num1} e ${num2} = ${num1 % num2}`)
-    console.log(`O incremento pre de ${num1} e ${num2} respectivamente é ${++num1} e ${++num2}`)
-    console.log(`O incremento pos de ${num1++} e ${num2++} respectivamente é ${num1} e ${num2}`)
-    console.log(`O decremento pre de ${num1} e ${num2} respectivamente é ${--num1} e ${--num2}`)
-    console.log(`O decremento pos de ${num1--} e ${num2--} respectivamente é ${num1} e ${num2}`)
+        console.log(`A soma entre ${num1} e ${num2} = ${num1 + num2}`)
+        console.log(`A subtração entre ${num1} e ${num2} = ${num1 - num2} `)
+        console.log(`A multiplicação entre ${num1} e ${num2} = ${num1 * num2}`)
+        console.log(`A divisão entre ${num1} e ${num2} = ${num1 / num2}`)
+        console.log(`O modulo entre ${num1} e ${num2} = ${num1 % num2}`)
+        console.log(`O incremento pre de ${num1} e ${num2} respectivamente é ${++num1} e ${++num2}`)
+        console.log(`O incremento pos de ${num1++} e ${num2++} respectivamente é ${num1} e ${num2}`)
+        console.log(`O decremento pre de ${num1} e ${num2} respectivamente é ${--num1} e ${--num2}`)
+        console.log(`O decremento pos de ${num1--} e ${num2--} respectivamente é ${num1} e ${num2}`)
+        console.log()
    
+//outra forma de usar os operadores
+
+    var teste = 10
+
+    console.log(`Soma ${teste += 5}`)
+    console.log(`Subtração ${teste -= 2}`)
+    console.log(`Multiplicação ${teste *= 10}`)
+    console.log(`Divisão ${teste /= 8}`)
+    console.log(`Modulo ${teste %= 6}`)
+    
