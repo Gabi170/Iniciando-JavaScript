de8b6c2945e24fd73bbeb3efbbb3bfcc2af5e437
Gabi170 <biele2611@gmail.com>
2023-02-08 22:54:25 -0300

    Continuando projeto Orçamento Pessoal

--- JS-Orientacao-a-Objetos/App_Orçamento_Pessoal/script.js
+++ JS-Orientacao-a-Objetos/App_Orçamento_Pessoal/script.js
@@ -8,7 +8,64 @@ class Despesa{
         this.valor = valor
         this.descricao = descricao
     }
+
+    validarDados(){
+
+        for(let i in this){
+            if(this[i] == undefined || this[i] == '' || this[i] == null){
+                return false
+            }
+        }
+
+        return true
+    }
 }
+
+class Bd{
+
+    constructor(){
+       let id = localStorage.getItem('id')
+
+        if(id === null){
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProcimoId(){
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d){
+        let id = this.getProcimoId()
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros(){
+
+        let despesas = Array()
+        
+        let id = localStorage.getItem('id')
+
+        for(let i = 1; i <= id; i++){
+
+            let despesa = JSON.parse(localStorage.getItem(i))
+            console.log(i, despesa)
+
+            if(despesa == null){
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+        return despesas
+    }
+
+  
+}
+
+let bd = new Bd()
 
 function cadastrarDespesa(){
 
@@ -25,13 +82,40 @@ let despesa = new Despesa(
     dia.value, 
     tipo.value, 
     valor.value, 
-    descricao.value)
+    descricao.value
+    )
 
-    gravar(despesa)
+    if(despesa.validarDados()){
+    //dialog de susseso
+
+        bd.gravar(despesa)
+        $('#registrarDespesa').modal('show')
+
+        document.getElementById('title').innerHTML = "Formulario enviado"
+        document.getElementById('frase').innerHTML = ' Formulario inserido com susseso, obrigado por usar nosso site.<br>😁😁😁'
+        document.getElementById('buttam').innerHTML = 'Voltar'
+        document.getElementById('buttam').className = 'btn btn-success'
+        document.getElementById('title').className = 'modal-header text-success'
+    }
+    else{
+    //dialog de erro  
+      $('#registrarDespesa').modal('show')
+
+      document.getElementById('title').innerHTML = "Erro ao enviar formulario"
+      document.getElementById('frase').innerHTML = 'Todos os campos são abrigatorios, tenha certeza de ter preenchido todos.<br>😰😰😰'
+      document.getElementById('buttam').innerHTML = 'Voltar e corrigir'  
+      document.getElementById('buttam').className = 'btn btn-danger'  
+      document.getElementById('title').className = 'modal-header text-danger'
+    }
 }
 
-function gravar(d){
-    localStorage.setItem('despesa', JSON.stringify(d))
+function carregaListaDespesa(){
+
+    let despesas = Array()
+
+    despesas = bd.recuperarTodosRegistros()
+
+    console.log(despesas)
 }
 
 
